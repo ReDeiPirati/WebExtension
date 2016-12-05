e6d799020578388fafef4c2d2b77f728f55dfab8
ReDeiPirati <alessio.gozzoli@gmail.com>
2016-12-05 19:09:27 +0100

    Add feature Last line, Positive and Negative examples

--- createDataset/popup/y_or_n.js
+++ createDataset/popup/y_or_n.js
@@ -38,6 +38,12 @@ function appendInDataset(choice){
   }
 }
 
+
+$.get("http://localhost/index.php")
+  .done(function( data ) {
+    $("#lastline").text(data);
+  });
+
 /*
 Listen for clicks in the popup.
 
--- createDataset/server/index.php
+++ createDataset/server/index.php
@@ -1,8 +1,18 @@
 <?php
-	//echo $_POST['s'];
 	// Path to file dataSet
-	$file = 'dataSet.txt';
-	// Check that POST is Set 
+	$file = "dataSet.txt";
+	
+	// Check that GET is SET
+	if (isset($_GET)){
+		// Execute tail -n 1 dataSet.txt(return last row)
+		$line = `tail -n 1 $file`;
+		$tot =`wc -l $file | cut -d " " -f1`;
+		$pos = `cat $file | rev | sort -nr | cut -d " " -f1 | uniq -c  | rev | head -n1 | cut -d " " -f2`;
+		$neg = `cat $file | rev | sort -nr | cut -d " " -f1 | uniq -c  | rev | tail -n1 | cut -d " " -f2`;
+
+		echo "Last line:\n". $line ."\nPositive: " . $pos . '/' . $tot . "Negative: ". $neg . "/" . $tot;
+	} 
+	// Check that POST is Set
 	if (isset($_POST['s'])) {
 		// Get Post parameter s
 		$line = $_POST['s'] . "\n";
