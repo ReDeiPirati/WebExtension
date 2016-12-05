bc83c1c2f6f88b5583e1f905c754a3ce21f98eb0
ReDeiPirati <alessio.gozzoli@gmail.com>
2016-12-05 19:50:45 +0100

    fix, but it is buggy: No controll over url with 2 different type of classification and no check over existences of pos and neg examples at the begining

--- createDataset/server/index.php
+++ createDataset/server/index.php
@@ -7,18 +7,31 @@
 		// Execute tail -n 1 dataSet.txt(return last row)
 		$line = `tail -n 1 $file`;
 		$tot =`wc -l $file | cut -d " " -f1`;
+		# At the beginning(pos and neg are equals cause there is no check over existence)
 		$pos = `cat $file | rev | sort -nr | cut -d " " -f1 | uniq -c  | rev | head -n1 | cut -d " " -f2`;
 		$neg = `cat $file | rev | sort -nr | cut -d " " -f1 | uniq -c  | rev | tail -n1 | cut -d " " -f2`;
 
-		echo "Last line:\n". $line ."\nPositive: " . $pos . '/' . $tot . "Negative: ". $neg . "/" . $tot;
+		echo "Last line:\n". $line ."\nPositive: " . $pos . "Negative: ". $neg . "Total Example: " . $tot;
+
 	} 
 	// Check that POST is Set
 	if (isset($_POST['s'])) {
-		// Get Post parameter s
-		$line = $_POST['s'] . "\n";
-		// using the FILE_APPEND flag to append the content to the end of the file
-		// and the LOCK_EX flag to prevent anyone else writing to the file at the same time
-		file_put_contents($file, $line, FILE_APPEND | LOCK_EX);
+		// FROM txt to LIST
+		$string = file_get_contents($file);
+		$list = explode("\n", $string); // \n is the character for a line break
+
+		// Do not duplicate(KEEP ATTENTION:the same url could be inserted as 0 and as 1!!!)
+		if(in_array($_POST['s'], $list)){
+			// DO NOTHING;
+			;
+		} else {
+			// Get Post parameter s
+			$line =  $_POST['s'] . "\n";
+			// using the FILE_APPEND flag to append the content to the end of the file
+			// and the LOCK_EX flag to prevent anyone else writing to the file at the same time
+			file_put_contents($file, $line, FILE_APPEND | LOCK_EX);
+		}
+		
 	}
 
 ?>
